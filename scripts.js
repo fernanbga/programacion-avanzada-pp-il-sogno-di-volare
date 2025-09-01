804556f0ca9ce69868349f62954d80d9a70ccc21
fernanbga <fernanburgos@icloud.com>
2025-09-01 21:53:44 +0200

    oke

--- scripts.js
+++ scripts.js
@@ -32,34 +32,56 @@ const militaryHelicopter = [
 
 class Gallery {
     constructor(civilImages, militaryImages) {
+        this.civilImages = civilImages; //array civ
+        this.militaryImages = militaryImages; //array mil
     }
-    
+
     getRandomCivil() {
+        const i = Math.floor(Math.random() * this.civilImages.length);
+        return this.civilImages[i];
     }
-    
+
     getRandomMilitary() {
+        const i = Math.floor(Math.random() * this.militaryImages.length);
+        return this.militaryImages[i];
     }
-    
+
     getAll() {
+        return [...this.civilImages, ...this.militaryImages];
     }
 }
 
 class Painter {
     constructor() {
+        this.createGallery();
     }
 
     createGallery() {
+        const section = document.createElement("section");
+        section.classList.add("gallery");
+        document.body.appendChild(section);
+        this.gallery = section;
     }
 
     createImageTag(imageUrl) {
+        const picture = document.createElement("picture");
+        const img = document.createElement("img");
+        img.src = imageUrl;
+        img.alt = "Img";
+        picture.appendChild(img);
+        return picture;
     }
 
     paintSingleImage(imageUrl) {
+        const element = this.createImageTag(imageUrl)
+        this.gallery.appendChild(element);
     }
 
     paintMultipleImages(arrayOfImages) {
+        arrayOfImages.forEach(img => this.paintSingleImage(img));
     }
 }
+
 
 const aircrafts = new Gallery(civilAircrafts, militaryAircrafts);
 const helicopters = new Gallery(civilHelicopters, militaryHelicopter);
